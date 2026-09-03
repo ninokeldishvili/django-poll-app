1d5f9991b7f72b1f12f69722042797b9f9e3587a
Nino Keldishvili <Ninokeldishvili@gmail.com>
2026-09-03 15:04:26 +0400

    Write tests for detail view

--- polls/tests.py
+++ polls/tests.py
@@ -79,4 +79,15 @@ class QuestionIndexViewTests(TestCase):
 
 
 
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        future_question  = create_question("Future question", days=5)
+        url = reverse('polls:detail', args=(future_question .id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
 
+    def test_past_question(self):
+        past_question = create_question("Past question", days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
